c7b0e9b6ef3656ad553e93fc824878c5025e047a
Bao Nguyen <Minzekatzebhn@gmail.com>
2023-03-07 17:57:30 +0100

    connect things

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import useContentful from "./useContentful";
+import RecipeCard from "./RecipeCard";
 
 const App = () => {
-  return <div>Hello</div>;
+  const { getRecipes } = useContentful();
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    getRecipes().then((response) => {
+      console.log(response);
+      setRecipes(response);
+    });
+  }, []);
+
+  return (
+    <div>
+      {recipes.map((recipe, index) => (
+        <RecipeCard key={index} recipe={recipe} />
+      ))}
+    </div>
+  );
 };
 
 export default App;
--- src/RecipeCard.js
+++ src/RecipeCard.js
@@ -1,7 +1,32 @@
 import React from "react";
 
-const RecipeCard = () => {
-  return <div></div>;
+const RecipeCard = ({ recipe }) => {
+  return (
+    <div>
+      <img
+        src={
+          "https:" +
+          recipe.recipeImg.file.url +
+          "?fit=thumb&f=top_left&h=200&w=200&r=180"
+        }
+      />
+      <h2>{recipe.recipeTitle}</h2>
+      <p>{recipe.shortDescription}</p>
+      <p>{recipe.longDescription}</p>
+      <h3>Ingredients</h3>
+      <ul>
+        {recipe.ingredient.map((item) => (
+          <li>{item}</li>
+        ))}
+      </ul>
+      <h3>Steps</h3>
+      <ul>
+        {recipe.steps.map((step) => (
+          <li>{step}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default RecipeCard;
