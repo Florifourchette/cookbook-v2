a408a8e4040e9a16dc319d548ddcbddb3c1fa9c3
Bao Nguyen <Minzekatzebhn@gmail.com>
2023-03-08 18:06:13 +0100

    debugged

--- src/RecipeCard.js
+++ src/RecipeCard.js
@@ -6,22 +6,22 @@ const RecipeCard = ({ recipe }) => {
       <img
         src={
           "https:" +
-          recipe.recipeImg.file.url +
+          recipe.fields.recipePicture?.fields.file.url +
           "?fit=thumb&f=top_left&h=200&w=200&r=180"
         }
       />
-      <h2>{recipe.recipeTitle}</h2>
-      <p>{recipe.shortDescription}</p>
-      <p>{recipe.longDescription}</p>
+      <h2>{recipe.fields.recipeTitle}</h2>
+      <p>{recipe.fields.shortDescription}</p>
+      <p>{recipe.fields.longDescription}</p>
       <h3>Ingredients</h3>
       <ul>
-        {recipe.ingredient.map((item) => (
+        {recipe.fields.ingredient?.map((item) => (
           <li>{item}</li>
         ))}
       </ul>
       <h3>Steps</h3>
       <ul>
-        {recipe.steps.map((step) => (
+        {recipe.fields.steps?.map((step) => (
           <li>{step}</li>
         ))}
       </ul>
