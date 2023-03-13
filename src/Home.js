7b3b7263ffb7858f655f772616af95abd448f924
Bao Nguyen <Minzekatzebhn@gmail.com>
2023-03-13 18:38:35 +0100

    card styling

--- src/Home.js
+++ src/Home.js
@@ -3,10 +3,14 @@ import RecipeCard from "./RecipeCard";
 
 export default ({ filteredRecipes }) => {
   return (
-    <div>
-      {filteredRecipes?.map((recipe) => (
-        <RecipeCard key={recipe.recipeUrl} recipe={recipe} />
-      ))}
+    <div className="container-fluid">
+      <div className="row d-flex justify-content-center">
+        <div className="col-sm-12 col-md-10 col-lg-8 col-xl-3 recipeCards d-flex justify-content-center flex-wrap w-100">
+          {filteredRecipes?.map((recipe) => (
+            <RecipeCard key={recipe.recipeUrl} recipe={recipe} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
