363d9104a412626adbce395901d04702e0f8c38c
Florifourchette <florifourchette@gmail.com>
2023-03-17 11:53:46 +0100

    redirection working

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { AuthProvider } from "./contexts/AuthContext";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter baseUrl="/cookbook-v2">
       <AuthProvider>
         <App />
       </AuthProvider>
